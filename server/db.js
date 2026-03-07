b97e4592cdbaf1259514233d399bc6f7f6310d41
Markus Hartley <markus.hartley@gmail.com>
2026-03-07 14:50:38 -0500

    Add Render persistent disk config for SQLite data survival

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const DB_PATH = join(__dirname, 'farts.db')
+const DB_PATH = process.env.DB_PATH || join(__dirname, 'farts.db')
 
 const db = new Database(DB_PATH)
 
